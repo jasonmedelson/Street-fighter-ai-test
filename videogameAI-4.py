90e290ae6ee70b5aef5f0e6e5ebde77c961baa4c
jedelson <edelson.m.jason@gmail.com>
2018-08-03 17:43:58 -0700

    roi is full screen so skipped

--- videogameAI-4.py
+++ videogameAI-4.py
@@ -8,15 +8,15 @@ def process_img(original_image):
 ##    processed_img = cv2.cvtColor(original_image,cv2.COLOR_BGR2GRAY)
     processed_img = cv2.Canny(original_image, threshold1=100, threshold2=300)
     return processed_img
-
+window_name = "StreetFighterV"
+# cv2.namedWindow( window_name, cv2.WINDOW_AUTOSIZE )
 last_time = time.time()
 while(True):
     screen = np.array(ImageGrab.grab(bbox=(0,40,1400,800)))
     new_screen = process_img(screen)
     print('Loop took {} seconds'.format(time.time()-last_time))
     last_time = time.time()
-    cv2.imshow('window', new_screen)
-    # cv2.imshow('window',screen)
+    cv2.imshow(window_name, new_screen)
 ##    cv2.imshow('window2',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
     if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
