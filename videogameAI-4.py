b3efbcd7d6fa3c5e6bc2dac5d9037f55b3275c00
jedelson <edelson.m.jason@gmail.com>
2018-08-03 18:00:03 -0700

    hough lines and guassian blur

--- videogameAI-4.py
+++ videogameAI-4.py
@@ -3,13 +3,26 @@ from PIL import ImageGrab
 import cv2
 import time
 
+def draw_lines(img,lines):
+    try:
+        for line in lines:
+            coords = line[0]
+            cv2.line(img,(coords[0],coords[1]), (coords[2],coords[3]), [255,255,255],2) #(img, (x1,y1),(x2,y2), color of line, line thickness)
+    except:
+        pass
+
 
 def process_img(original_image):
 ##    processed_img = cv2.cvtColor(original_image,cv2.COLOR_BGR2GRAY)
     processed_img = cv2.Canny(original_image, threshold1=100, threshold2=300)
+    cv2.GaussianBlur(processed_img, (5,5),0)
+    lines = cv2.HoughLinesP(processed_img, 1 ,np.pi/180, 100, 5, 5) #(image, 1, np.pi/180, 100, min line length, line gap fill)
+    draw_lines(processed_img,lines)
+
+
     return processed_img
 window_name = "StreetFighterV"
-# cv2.namedWindow( window_name, cv2.WINDOW_AUTOSIZE )
+cv2.namedWindow( window_name, cv2.WINDOW_AUTOSIZE )
 last_time = time.time()
 while(True):
     screen = np.array(ImageGrab.grab(bbox=(0,40,1400,800)))
