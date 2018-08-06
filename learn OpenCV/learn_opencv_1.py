868a64c4fec68a8edf04ec33edcefd1ebe28f383
jedelson <edelson.m.jason@gmail.com>
2018-08-06 12:45:40 -0700

    pathlib to handle file pathing, cv2 and plt for display

--- learn OpenCV/learn_opencv_1.py
+++ learn OpenCV/learn_opencv_1.py
@@ -1,3 +1,18 @@
 import cv2
+import pathlib
 import matplotlib.pyplot as plt
-import numpy as np
+
+basePath =  pathlib.Path.cwd()
+# print(basePath)
+fileName = basePath / 'learn OpenCV/images/smallbeach.jpg'
+# print(type(fileName.resolve()))
+# print(fileName.e1ists())
+# img = cv2.imread("C:\\Users\\Jason\\Desktop\\machine learning ai\\learn OpenCV\\images\\smallbeach.jpg", cv2.IMREAD_GRAYSCALE)
+img = cv2.imread(str(fileName), cv2.IMREAD_GRAYSCALE)
+
+# cv2.imshow('image', img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+plt.imshow(img, cmap='gray', interpolation='bicubic')
+plt.show()
